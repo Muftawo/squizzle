5bc7369b21116894fa0bc632f329227e7b415910
Muftawo <hajjmuf@gmail.com>
2023-10-31 00:04:40 +0000

    updated context

--- src/script.js
+++ src/script.js
@@ -4,15 +4,24 @@ window.addEventListener('load', function () {
     canvas.width = 1280;
     canvas.height = 720;
 
+    ctx.fillStyle = 'white';
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = 'white';
+
+
     class Player {
         constructor(game) {
             this.game = game;
+            this.collisionX = this.game.width * 0.5;
+            this.collisionY = this.game.height * 0.5;
+            this.collisionRadius = 30;
+
 
         }
         draw(context){
             context.beginPath();
-            context.arc(400, 300 , 50 , 0 , Math.PI * 2)
-            context.fill();
+            context.arc(this.collisionX, this.collisionY , this.collisionRadius , 0 , Math.PI * 2)
+            context.stroke();
         }
     }
     class Game {
@@ -31,6 +40,7 @@ window.addEventListener('load', function () {
     }
 
     const game = new Game(canvas)
+    game.render(ctx)
     console.log(game)
 
     function animate() {
